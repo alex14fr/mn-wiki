9c7dc33441bd5be34bd69fb867f7ef6ecd643243
alexandre <alex14fr@gmail.com>
2020-02-14 20:12:36 +0100

    x

---
+++ static/script.js
@@ -0,0 +1,14 @@
+function switchMenuMobile() {
+	var lfsb=document.getElementById("leftsidebar");
+	if(lfsb.style.display=="" || lfsb.style.display=="none") {
+		lfsb.style.display="block";
+		document.getElementById("mobilemenulink").innerHTML="Hide menu";
+	} else {
+		lfsb.style.display="none";
+		document.getElementById("mobilemenulink").innerHTML="Show menu";
+	}
+}
+window.addEventListener('DOMContentLoaded',function() {
+					document.getElementById('mobilemenulink').addEventListener('click', switchMenuMobile);
+});
+
