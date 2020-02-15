d4a1804ad659c6ac261e8ed9aed846bb872722f0
alexandre <alex14fr@gmail.com>
2020-02-15 12:55:13 +0100

    x

--- doku.php
+++ doku.php
@@ -167,8 +167,8 @@ IP:           ".$clientIp."
 
 Summary:      ".$ps."
 
-- Old revision: ".$baseUrl."?id=$pageId&rev=$oldmt
-+ New revision: ".pageLink($pageId,true)."
+--- Old revision: ".(empty($oldmt) ? "page created" : $baseUrl."?id=$pageId&rev=$oldmt")."
++++ New revision: ".pageLink($pageId,true)."
 
 ".san_diff(textDiff($oldtext,$newtext)));
 			
