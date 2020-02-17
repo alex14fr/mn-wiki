c7f4643099484d3a9f8f479e85c8702c3e6094ce
alexandre <alex14fr@gmail.com>
2020-02-17 20:52:51 +0100

    x

--- auth.php
+++ auth.php
@@ -1,4 +1,35 @@
 <?php
+/**
+
+BSD 2-Clause License
+
+Copyright (c) 2020, Alexandre Janon <alex14fr@gmail.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+
 include_once "conf/conf.php";
 
 function auth_getline($login) {
@@ -114,9 +145,12 @@ function generatePassword($length = 8) {
 function auth_changeUser($login, $newline) {
 	global $pwdFile;
 	$newfile="";
-	$fd=fopen($pwdFile,"r");
+	$fd=fopen($pwdFile,"r+");
 	if(!$fd) {
-		die("can't open passwd file");
+		die("can't open passwd file in r+ mode");
+	}
+	if(!flock($fd,LOCK_EX)) {
+		die("can't lock passwd file");
 	}
 	while($line=fgets($fd)) {
 		$lspl=explode(':',$line);
@@ -125,8 +159,11 @@ function auth_changeUser($login, $newline) {
 		}
 	}
 	$newfile.=$newline;
-	if(!is_writable($pwdFile)) { die("can't open passwd file for writing"); }
-	file_put_contents($pwdFile,$newfile);
+	ftruncate($fd,0);
+	rewind($fd);
+	fwrite($fd,$newfile);
+	flock($fd,LOCK_UN);
+	fclose($fd);
 }
 
 function auth_changePassword($login, $newpass) {
