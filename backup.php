dff104fa39322afcf5dffaf1ed585e519dc71467
alexandre <alex14fr@gmail.com>
2020-02-20 11:13:30 +0100

    x

--- backup.php
+++ backup.php
@@ -37,7 +37,7 @@ if(!empty($allowedIp) && $_SERVER['HTTP_X_FORWARDED_FOR']!=$allowedIp){ die("E")
 
 if(!empty($_POST['f']) && !empty($_POST['tok']) && !empty($_POST['time'])) {
 	$clntTime=preg_replace('/[^0-9]/','',$_POST['time']);
-	if( abs($clntTime - time()) > 20 ) { die("desync"); }
+	if( abs($clntTime - time()) > 90 ) { die("desync"); }
 	$hashok=hash_hmac("sha256", $_POST['f'], $secret.$clntTime);
 	if(!hash_equals($hashok, $_POST['tok'])) { die("E"); }
 
