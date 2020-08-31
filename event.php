8be9ada2944e2f468b4ff76e8396704ff2192b5d
alexandre <alex14fr@gmail.com>
2020-08-31 19:12:41 +0200

    x

--- event.php
+++ event.php
@@ -208,7 +208,7 @@ if ($_GET['action']=='remove') {
 	if(!hash_equals(unsubToken($_GET['nompre'],$_GET['mail']), $_GET['sectok'])) {
 		die("E");
 	}
-	if(!$db->exec("DELETE FROM inscrits WHERE nomprenom='".$db->escapeString(ucname(strtolower($_GET['nompre'])))."' AND mail='".$db->escapeString($_GET['mail'])."'")) {
+	if(!$db->exec("DELETE FROM inscrits WHERE id='".$db->escapeString($id)."' AND nomprenom='".$db->escapeString(ucname(strtolower($_GET['nompre'])))."' AND mail='".$db->escapeString($_GET['mail'])."'")) {
 		die("db error delete ".$db->lastErrorMsg());
 	} else {
 		print "Your registration to $id has been canceled. ";
