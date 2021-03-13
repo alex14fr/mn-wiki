f801102c3f1d9df91d21a58de7f25935ef22be21
alexandre <alex14fr@gmail.com>
2021-03-13 01:11:38 +0100

    x

--- 21upload.php
+++ 21upload.php
@@ -34,7 +34,7 @@ if($_FILES['f']) {
 	}
 	print "<b>".nl2br($str)."</b><hr>";
 	//mail("alex14fr@gmail.com","mascot21 upload ".$_GET['mail'],$str."\n".$str2);
-	$ff=popen("/usr/sbin/sendmail alexandre.janon@u-psud.fr","w");
+	$ff=popen("/usr/sbin/sendmail alex14fr@gmail.com","w");
 	fwrite($ff,"subject: mascot21 upload ".$_GET['mail']."\n\n".$str."\n".$str2."\n");
 	pclose($ff);
 
