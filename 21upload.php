78f9e965bb0b29d93595d53bb989b72c25a1391f
alexandre <alex14fr@gmail.com>
2021-03-13 03:09:45 +0100

    x

--- 21upload.php
+++ 21upload.php
@@ -8,7 +8,6 @@ $upload_dir="/persist/mascot21_upload/$mail/";
 
 if(!hash_equals(sha1($sec.$mail), $tok)) {
 	print 'token error '; 
-//	print sha1($sec.$mail);
 	exit;
 } 
 
@@ -18,31 +17,20 @@ if(!in_array($mail,file($allowed_file,FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINE
 
 session_start();
 
-if($_FILES['f']) {
+if($_GET['fn']) {
 	@mkdir($upload_dir);
 	$str="";$str2="";
-	foreach($_FILES['f']['error'] as $key=>$err) {
-		if($err==UPLOAD_ERR_OK) {
-			$fnam=bin2hex(random_bytes(32));
-			file_put_contents($upload_dir.$fnam."_name", $_FILES['f']['name'][$key]);
-			$muf=move_uploaded_file($_FILES['f']['tmp_name'][$key],$upload_dir.$fnam);
-			if(!$muf) 
-				$str.="Move error during upload of ".$_FILES['f']['name'][$key]."\n";
-			else {
-				$str.="Upload of ".$_FILES['f']['name'][$key]." succeeded. \n";
-				$str2.="https://gdr-mascotnum.fr/21admin.php?f=".urlencode($_GET['mail']."/".$fnam)."\n";
-			}
-		} else if($err != 4)
-			$str.="Error during upload of ".$_FILES['f']['name'][$key]." : ".$err."\n";
-	}
-	print "<b>".nl2br($str)."</b><hr>";
+	$fnam=bin2hex(random_bytes(32));
+	stream_copy_to_stream(fopen("php://input","r"),fopen($upload_dir.$fnam,"w"));
+	file_put_contents($upload_dir.$fnam."_name", $_GET['fn']);
+	$str.="Upload of ".$_FILES['f']['name'][$key]." succeeded. \n";
+	$str2.="https://gdr-mascotnum.fr/21admin.php?f=".urlencode($_GET['mail']."/".$fnam)."\n";
+	print 'ok';
 	$ff=popen("/usr/sbin/sendmail $mailAdmin","w");
 	fwrite($ff,"subject: mascot21 upload ".$_GET['mail']."\n\n".$str."\n".$str2."\n");
 	pclose($ff);
-
+	exit;
 }
-
-
 ?>
 <!doctype html>
 <meta name=viewport value="width=device-width">
@@ -51,23 +39,39 @@ if($_FILES['f']) {
 To prepare for the poster session of the MASCOT21 conference, we ask you to send to us a video recording containing your presentation (of <i>5 minutes maximum</i>) before <b>XXXX</b>.<p>
 You can send to us one video file (of you presenting your poster, or a screen presentation with aural comments, or both), or two synchronized video files (one with you, the other one for your screen presentation). You can <b>(must ?)</b> also send a PDF file of your poster that will serve as a support during audience's questions session.<p>
 You can prepare the video files using your favorite movie recording application, or you can use <a href="21videorec.php?mail=<?php print $_GET['mail'] ?>&tok=<?php print $_GET['tok'] ?>">this video recording web application</a>.<p>
-Please use the following form to send your file(s):
-<form id=form enctype=multipart/form-data method=post>
-<input type=file name=f[]><br>
-<input type=file name=f[]><br>
-<input type=file name=f[]><br>
-<input type=submit value=Upload>
-<input type="hidden" name="<?php echo ini_get("session.upload_progress.name"); ?>" value="1">
+Please use the following form to send your file:
+<form>
+<input id=f type=file>
+<input id=submit type=button value=Submit>
 </form>
-<i>You may send multiple files at once provided the overall size of the files is not too large; if so, you can use this form multiple times for each file.</i>
 <div id=progress></div>
 <script>
-/*
-document.getElementById('form').onsubmit=function(ev) {
-	setInterval( function() {  
-		fetch('/21progress.php').then(resp=>resp.text())
-										.then(data=>document.getElementById('progress').innerHTML=data);
-		}, 2000); 
-} */
+var sendurl="/21upload2.php?mail=<?php print urlencode($mail); ?>&tok=<?php print urlencode($tok); ?>";
+var xhr;
+
+function updProgress(ev) {
+	document.getElementById('progress').innerHTML='uploaded '+ev.loaded+' / '+ev.total+' bytes ('+(100*ev.loaded/ev.total)+' %)';
+}
+
+function updError(ev) {
+	document.getElementById('progress').innerHTML='error : '+xhr.status;
+}
+
+function updOK(ev) {
+	document.getElementById('progress').innerHTML='file upload successful';
+}
+
+document.getElementById('submit').onclick=function(ev) {
+	document.getElementById('progress').innerHTML='uploading...';
+	var ff=document.getElementById('f');
+	var x=ff.files[0];
+	var sendurl2=sendurl+'&fn='+encodeURIComponent(x.name);
+	var xhr=new XMLHttpRequest();
+	xhr.upload.onprogress=updProgress;
+	xhr.upload.onerror=updError;
+	xhr.upload.onload=updOK;
+	xhr.open('POST',sendurl2);
+	xhr.send(x);
+};
 </script>
 
