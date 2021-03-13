ce99a757e6dfe0673decd696452de42b91aab9b0
alexandre <alex14fr@gmail.com>
2021-03-13 03:41:25 +0100

    x

--- 21view.php
+++ 21view.php
@@ -4,6 +4,8 @@ if(!hash_equals(sha1(file_get_contents("/persist/data/secret1")."fil".$f),$_GET[
 	print 'tok error';
 	exit;
 }
+if(strpos($f,"..")!==false) { print 'path error'; exit; }
+if($_GET['del']) { unlink('/persist/mascot21_upload/'.$f); print 'ok deleted'; exit; }
 header('content-type: application/octet-stream');
 header('content-disposition: attachment;filename="'.file_get_contents('/persist/mascot21_upload/'.$f.'_name').'"');
 readfile('/persist/mascot21_upload/'.$f);
