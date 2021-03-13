bdbfa4b5a3ae63baf8ee7987911873f63c6ee656
alexandre <alex14fr@gmail.com>
2021-03-13 02:07:23 +0100

    x

--- 21upload.php
+++ 21upload.php
@@ -50,7 +50,7 @@ if($_FILES['f']) {
 <h2>MASCOT21 Days - Poster session - Upload files</h2>
 To prepare for the poster session of the MASCOT21 conference, we ask you to send to us a video recording containing your presentation (of <i>5 minutes maximum</i>) before <b>XXXX</b>.<p>
 You can send to us one video file (of you presenting your poster, or a screen presentation with aural comments, or both), or two synchronized video files (one with you, the other one for your screen presentation). You can <b>(must ?)</b> also send a PDF file of your poster that will serve as a support during audience's questions session.<p>
-You can prepare the video files using your favorite movie recording application, or you can use <a href="21record.php?mail=<?php print $_GET['mail'] ?>&tok=<?php print $_GET['tok'] ?>">this video recording web application</a>.<p>
+You can prepare the video files using your favorite movie recording application, or you can use <a href="21videorec.php?mail=<?php print $_GET['mail'] ?>&tok=<?php print $_GET['tok'] ?>">this video recording web application</a>.<p>
 Please use the following form to send your file(s):
 <form id=form enctype=multipart/form-data method=post>
 <input type=file name=f[]><br>
