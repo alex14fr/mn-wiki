7b3804a0f96f1a48d1354cdac1fd0589c8947421
alexandre <alex14fr@gmail.com>
2021-03-15 11:47:42 +0100

    x

--- 21admin.php
+++ 21admin.php
@@ -42,7 +42,7 @@ If you have any question regarding the process, you can send mail to alexandre.j
 
 Best regards,
 
-EOF)|msmtp -a psud <?php print $l; ?> alexandre.janon@u-psud.fr clementine.prieur@univ-grenoble-alpes.fr celine.helbert@ec-lyon.fr anthony.nouy@ec-nantes.fr christophette.blanchet@ec-lyon.fr
+EOF)|msmtp -a psud <?php print $l; ?> alexandre.janon@u-psud.fr bertrand.iooss@edf.fr clementine.prieur@univ-grenoble-alpes.fr celine.helbert@ec-lyon.fr anthony.nouy@ec-nantes.fr christophette.blanchet@ec-lyon.fr
 }
 ?>
 </textarea>
