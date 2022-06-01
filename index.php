380a103754b9d28553e17525c22d146ff2985dee
alexandre <alex14fr@gmail.com>
2022-06-01 17:31:46 +0200

    x

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ if (empty($_GET['do']) && empty($_POST['do'])) {
 		$ss1=substr($_SERVER['HTTP_IF_NONE_MATCH'],0,8);
 		$ss2=substr($etag,0,8);
 		if($ss1==$ss2) {
-			http_response_code(302);
+			http_response_code(304);
 			exit;
 		}
 	}
