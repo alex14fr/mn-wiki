3ac2f08b62a5644805d02b669ddb7106f565d250
alexandre <alex14fr@gmail.com>
2022-06-01 17:33:32 +0200

    x

--- index.php
+++ index.php
@@ -54,6 +54,7 @@ if (empty($_GET['do']) && empty($_POST['do'])) {
 	if(isset($_SERVER['HTTP_IF_NONE_MATCH'])) {
 		$ss1=substr($_SERVER['HTTP_IF_NONE_MATCH'],0,8);
 		$ss2=substr($etag,0,8);
+		header("X-aa: $ss1 $ss2");
 		if($ss1==$ss2) {
 			http_response_code(304);
 			exit;
