253899c28f46fa36ee1ba8142f925b607acdb5c6
alexandre <alex14fr@gmail.com>
2022-06-01 17:18:58 +0200

    x

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ if (empty($_GET['id'])) {
 
 if (empty($_GET['do']) && empty($_POST['do'])) {
 	$etag=md5(get_login().$pageId.file_get_contents("commit_id").filemtime($pageDir."/$pageId.txt"));
-	if(isset($_SERVER['HTTP_IF_NONE_MATCH']) && $etag === explode("-",$_SERVER['HTTP_IF_NONE_MATCH'])[0]) {
+	if(isset($_SERVER['HTTP_IF_NONE_MATCH']) && substr($etag,0,8) === substr(explode("-",$_SERVER['HTTP_IF_NONE_MATCH'])[0],0,8)) {
 		header("302 Not modified");
 		exit;
 	}
