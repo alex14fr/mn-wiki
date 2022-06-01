5a11d0f5179607690b818ca5ddbc69b52063d1d8
alexandre <alex14fr@gmail.com>
2022-06-01 17:54:56 +0200

    x

--- index.php
+++ index.php
@@ -55,6 +55,7 @@ if (empty($_GET['do']) && empty($_POST['do'])) {
 		$ss1=substr($_SERVER['HTTP_IF_NONE_MATCH'],0,10);
 		$ss2=substr($etag,0,10);
 		if($ss1==$ss2) {
+			header("Cache-control: private");
 			http_response_code(304);
 			exit;
 		}
