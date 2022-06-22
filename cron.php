b204afa54cbf9aa85a2a2ea2c8d350264409d845
alexandre <alex14fr@gmail.com>
2022-06-22 12:06:05 +0200

    x

--- cron.php
+++ cron.php
@@ -160,7 +160,7 @@ function updhal()
 }
 
 
-$t = fetchurl("https://mascot-num.blogspot.com/feeds/posts/default");
+$t = fetchurl("https://mascot-num.blogspot.com/feeds/posts/default", true);
 file_put_contents("ephemeral/feedext.rss", $t);
 file_put_contents("ephemeral/rss2.html", lastnews2());
 file_put_contents("ephemeral/rssshort.html", lastshort());
