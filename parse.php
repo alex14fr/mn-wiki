e7075139c77089d5671fa2997a99740181cb85ee
alexandre <alex14fr@gmail.com>
2022-08-11 12:33:37 +0200

    x

--- parse.php
+++ parse.php
@@ -339,7 +339,9 @@ function parse_line($l)
 							   $pageIdx=$pageId;
 								$pageId="software_updates";
 								$fp=fopen("data/pages/software_updates.txt","r");
-								do $l=fgets($fp, 1024) while($l!==false && !preg_match("/^\s*\*\s*/",$l));
+								do {
+									$l=fgets($fp, 1024);
+								} while($l!==false && !preg_match("/^\s*\*\s*/",$l));
 								$iii=0;
 								while($iii++<5 && $l!==false) {
 									$out.=parse_line($l);
