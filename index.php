b93c0214e28b384a159695f009b6851756e5e231
alexandre <alex14fr@gmail.com>
2022-08-18 07:06:59 +0200

    x

--- index.php
+++ index.php
@@ -317,7 +317,7 @@ New: " . pageLink($pageId, true) . "
 				$txtbasehtml=nl2br($txtbase);
 				$dc=contextDiff(Diff::compare(san_diff($oldtext), san_diff($newtext)));
 
-				$reason=($pageId=="software_updates" ? "change_software" : "change");
+				$reason=(in_array("change_$pageId", $mailNotify) ? "change_$pageId" : "change");
             sendNotify($reason, "Page $pageId changed", "This is a multipart message in MIME format.
 
 --$boundary
