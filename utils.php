506c136d14bc78e217699db1369d8fd256571e2b
alexandre <alex14fr@gmail.com>
2023-06-25 12:25:44 +0200

    x

--- utils.php
+++ utils.php
@@ -130,7 +130,8 @@ function readtmpl($id)
     if ($id === "htmlhead") {
         $out = str_replace("<!doctype html>", "<!doctype html>\n", $out);
 		  $out = str_replace("~~CSSTS~~", filemtime("static/style.min.css"), $out);
-		  if($id == '' || $id == 'index') {
+		  global $pageId;
+		  if($id == 'index') {
 			  global $metaDesc;
 			  $out = str_replace('~~METADESC~~', '<meta name=description content="'.$metaDesc.'">', $out);
 		  } else {
