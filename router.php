087b61bd31a670ad23c16a538b1149012f8492ea
Alexandre Janon <alex14fr@gmail.com>
2023-07-13 20:24:32 +0200

    router

--- router.php
+++ router.php
@@ -10,13 +10,23 @@ if(strlen($req)==5 && substr($req,0,3)=='/20' && $req[3]>=0 && $req[3]<=9 && $re
 	$id='mascot'.substr($req,1);
 } else if($req=='/dam.incertitudes') {
 	$id='forumincertitudes';
-} else if(strpos($req,'/',1)===false && strpos($req,'?')===false && strpos($req,'.')===false) {
-	$id=substr($req,1);
 } else {
+	$n=strlen($req);
+	for($i=1; $i<$n; $i++) 
+		if(! ($req[$i]>='a' && $req[$i]<='z') || 
+			  ($req[$i]>='A' && $req[$i]<='Z') ||
+			  ($req[$i]>='0' && $req[$i]<='9') ||
+			   $req[$i]=='_' || $req[$i]=='-' ) 
+			break;
+	if($i==$n)
+		$id=substr($req,1);
+}
+if($id===false) {
 	header('Status: 404');
 	print 'The requested resource has not been found. ';
 	exit;
+} else {
+	$_GET['id']=$id;
+	include 'index.php';
 }
-$_GET['id']=$id;
-include 'index.php';
 
