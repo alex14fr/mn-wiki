1f8974af071aa39f8ddb00e4e137daf59e88bf64
Alexandre Janon <alex14fr@gmail.com>
2024-03-25 20:56:49 +0100

    sendNotify()

--- utils.php
+++ utils.php
@@ -113,14 +113,7 @@ function san_diff($string)
 function sendNotify($reason, $subj, $body, $hdrs="")
 {
     global $mailNotify;
-
-    foreach ($mailNotify[$reason] as $to) {
-	//	 print "\n<!-- $to -->\n";
-        xmail($to, $subj, $body, $hdrs);
-    }
-	 //exit;
-
-	 file_put_contents($dataDir."/maillog","To: $to\r\nSubject: $subj\r\n$body\r\n\r\n",FILE_APPEND);
+    xmail(implode(',',$mailNotify[$reason]), $subj, $body, $hdrs);
 }
 
 function readtmpl($id)
